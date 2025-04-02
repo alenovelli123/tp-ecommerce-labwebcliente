9041e9f6a8b981b34984dee85d8c8ec35cf99e39
alenovelli123 <alejandro.novelli@gmail.com>
2025-04-02 15:05:42 -0300

    Update api.js
    cambiando api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 export async function getProducts() {
   try {
     const res = await fetch('https://fakestoreapi.com/products');
-    if (!res.ok) throw new Error('Error al obtener productos');
+    if (!res.ok) throw new Error('Error al obtener productos'); // no forma parte de la documentacion
     return await res.json();
   } catch (error) {
     console.error('Error:', error);
