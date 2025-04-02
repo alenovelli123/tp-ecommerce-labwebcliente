0c3f723dcd330c40c2071e9870e63731dcdca0c9
alenovelli123 <alejandro.novelli@gmail.com>
2025-04-02 09:47:30 -0300

    Update carrito.js
    se agrega offcanvas

--- src/carrito/carrito.js
+++ src/carrito/carrito.js
@@ -1,4 +1,14 @@
+
+// ✅ Esto va al principio del archivo carrito.js
+import { guardarEnStorage, obtenerDeStorage } from '../utils/storage.js';
+
+// ✅ Esta es tu función exportada
 export function agregarAlCarrito(producto) {
+  console.log("Agregando:", producto); // ✅
+  const carritoActual = obtenerDeStorage('carrito');
+  carritoActual.push(producto);
+  guardarEnStorage('carrito', carritoActual);
+  console.log("Nuevo carrito:", carritoActual); // ✅
   alert(`${producto.title} agregado al carrito`);
-  // Aquí se conectará con LocalStorage y se actualizará el carrito
 }
+
