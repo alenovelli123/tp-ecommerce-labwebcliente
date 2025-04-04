824f625eb3165655a82d0790f5084ec93070a4f5
alenovelli123 <alejandro.novelli@gmail.com>
2025-04-03 21:16:21 -0300

    💡 Ajustes de responsive para navbar, tarjetas y modal

--- src/carrito/carrito.js
+++ src/carrito/carrito.js
@@ -1,12 +1,17 @@
-
 import { guardarEnStorage, obtenerDeStorage } from '../utils/storage.js';
+import { alertaAgregadoAlCarrito } from '../utils/alerta.js';
 
 export function agregarAlCarrito(producto) {
-  console.log("Agregando:", producto); // ✅
-  const carritoActual = obtenerDeStorage('carrito');
-  carritoActual.push(producto);
-  guardarEnStorage('carrito', carritoActual);
-  console.log("Nuevo carrito:", carritoActual); // ✅
-  alert(`${producto.title} agregado al carrito`);
-}
+  const carrito = obtenerDeStorage('carrito');
+  const existente = carrito.find(p => p.id === producto.id);
 
+  if (existente) {
+    existente.cantidad++;
+  } else {
+    const nuevoProducto = { ...producto, cantidad: 1 };
+    carrito.push(nuevoProducto);
+  }
+
+  guardarEnStorage('carrito', carrito);
+  alertaAgregadoAlCarrito(producto);
+}
